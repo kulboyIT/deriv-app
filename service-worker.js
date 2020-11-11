3e69383bf35320bdc86339efff6b5cca6fe3e509
ci-build <ci-build@binary.com>
2020-11-11 04:15:11 +0000

    [skip ci]

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b055ff47f7b8be9c808c18fd201ae4c6.js"
+  "/precache-manifest.4dc18b665f5a9dc00635319caad9356c.js"
 );
 
 workbox.core.skipWaiting();
