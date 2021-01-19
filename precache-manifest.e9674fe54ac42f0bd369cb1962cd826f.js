711bd895c1e556ef3d0e2167752082a16c284ab9
ci-build <ci-build@binary.com>
2021-01-19 04:43:29 +0000

    [skip ci]

--- precache-manifest.e9674fe54ac42f0bd369cb1962cd826f.js
+++ precache-manifest.e9674fe54ac42f0bd369cb1962cd826f.js
@@ -236,7 +236,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/core.complaints-policy.7e2c0f77c21cf57e7638.css"
   },
   {
-    "revision": "3aee7020d52a175938d7",
+    "revision": "38200ae7c3c113ca35ff",
     "url": "/css/core.main.css"
   },
   {
@@ -800,8 +800,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/core.dashboard.2204d778e917b5cc83b8.js"
   },
   {
-    "revision": "3aee7020d52a175938d7",
-    "url": "/js/core.main.cf0ed306f09ac5a93295.js"
+    "revision": "38200ae7c3c113ca35ff",
+    "url": "/js/core.main.b21e74c087da7293aa5a.js"
   },
   {
     "revision": "0028cdff9393c805d137",
