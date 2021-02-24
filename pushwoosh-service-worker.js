9a73c6e57a0e5b6769de57e0a1cc596636f3fb9b
ci-build <ci-build@binary.com>
2021-02-24 07:34:40 +0000

    [skip ci]

--- pushwoosh-service-worker.js
+++ pushwoosh-service-worker.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line
-importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search);
+importScripts("https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js"+self.location.search);
